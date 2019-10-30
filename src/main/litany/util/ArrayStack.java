e8106853035c1fb2890b36931d9bb56f0918fd51
Eug Kenny <eugkenny@gmail.com>
2019-10-30 22:48:43 +0000

    Created ArrayStack

---
+++ src/main/litany/util/ArrayStack.java
@@ -0,0 +1,43 @@
+package litany.util;
+
+import java.util.Iterator;
+
+public class ArrayStack<E> implements Stack<E> {
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(E element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public E pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
