0a8edd2a276ddddab11e0a47cc27c10ee7334c21
Eug Kenny <eugkenny@gmail.com>
2020-10-13 13:32:28 +0100

    Initial Graph implementation

--- src/main/litany/util/Queue.java
+++ src/main/litany/util/Queue.java
@@ -7,47 +7,45 @@ package litany.util;
  * <li><b>enqueue</b>, which adds an element to the rear of the queue, and
  * <li><b>dequeue</b>, which removes an element from the front of the queue
  * </ul>
- * 
- * @author eugkenny
  *
  * @param <E>
+ * @author eugkenny
  */
 
-public interface Queue<E> extends Iterable<E>{
-	
-	/**
-	 * Enqueue (add) the element to the rear of the queue
-	 * 
-	 * @param element
-	 */
-	public void enqueue(E element);
-	
-	/**
-	 * Dequeue (remove) the element from the front of the queue
-	 * 
-	 * @return the element from the front of the queue
-	 */
-	public E dequeue();
-	
-	/**
-	 * Return the element from the top of the stack (without removing)
-	 * 
-	 * @return the element from the top of the stack
-	 */
-	public E peek();
-	
-	/**
-	 * Re whether this queue contains any elements.
-	 * 
-	 * @return {@code true} if this queue contains no elements, {@code false}
-	 *         otherwise
-	 */
-	public boolean isEmpty();
+public interface Queue<E> extends Iterable<E> {
 
-	/**
-	 * Returns the number of elements in this queue.
-	 * 
-	 * @return the number of elements in this queue
-	 */
-	public int size();
+    /**
+     * Enqueue (add) the element to the rear of the queue
+     *
+     * @param element
+     */
+	void enqueue(E element);
+
+    /**
+     * Dequeue (remove) the element from the front of the queue
+     *
+     * @return the element from the front of the queue
+     */
+    E dequeue();
+
+    /**
+     * Return the element from the top of the stack (without removing)
+     *
+     * @return the element from the top of the stack
+     */
+    E peek();
+
+    /**
+     * Returns whether this queue contains any elements.
+     *
+     * @return {@code true} if this queue contains no elements, {@code false} otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns the number of elements in this queue.
+     *
+     * @return the number of elements in this queue
+     */
+    int size();
 }
---
+++ src/main/litany/util/graph/Graph.java
@@ -0,0 +1,43 @@
+package litany.util.graph;
+
+public interface Graph<V,E> extends Iterable<V>{
+
+    /**
+     * The number of vertices in the graph
+     *
+     * @return the number of vertices in the graph
+     */
+    int order();
+
+    /**
+     * The number of edges in the graph
+     *
+     * @return the number of edges in the graph
+     */
+    int size();
+
+    /**
+     * Adds a vertex to the graph
+     *
+     * @param v the vertex to add to the graph
+     */
+    void addVertex(V  v);
+
+    /**
+     * Removes a vertex and all of it's incident edges
+     * @param v the vertex to remove from the graph
+     */
+    void removeVertex(V v);
+
+    /**
+     * Returns whether the specified vertex is present on the graph
+     * @return {@code true} if the specified vertex is on the graph, {@code false} otherwise
+     */
+    boolean hasVertex(V v);
+
+    void addEdge(V u, V v, E e);
+
+    boolean hasEdge(V u, V v);
+
+    Iterable<V> neighbours(V v);
+}
